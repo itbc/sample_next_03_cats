4075a0a675f8813948d84df519b410313c4c147f
Tomo <tomo.itbc.web@gmail.com>
2023-05-30 10:02:57 +0900

    commit 1

---
+++ pages/index.tsx
@@ -0,0 +1,44 @@
+import { NextPage } from "next";
+import { useEffect, useState } from "react";
+import styles from "./index.module.css";
+ 
+const IndexPage: NextPage = () => {
+  const [imageUrl, setImageUrl] = useState("");
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    fetchImage().then((newImage) => {
+      setImageUrl(newImage.url);
+      setLoading(false);
+    });
+  }, []);
+  // ボタンをクリックしたときに画像を読み込む処理
+  const handleClick = async () => {
+    setLoading(true); // 読込中フラグを立てる
+    const newImage = await fetchImage();
+    setImageUrl(newImage.url); // 画像URLの状態を更新する
+    setLoading(false); // 読込中フラグを倒す
+  };
+  return (
+    // <div>
+      // <button onClick={handleClick}>他のにゃんこも見る</button>
+    <div className={styles.page}>
+      <button onClick={handleClick} className={styles.button}>
+        他のにゃんこも見る
+      </button>
+      <div className={styles.frame}>
+        {loading || <img src={imageUrl} className={styles.img} />}
+      </div>
+    </div>
+  );
+};
+export default IndexPage;
+ 
+type Image = {
+  url: string;
+};
+const fetchImage = async (): Promise<Image> => {
+  const res = await fetch("https://api.thecatapi.com/v1/images/search");
+  const images = await res.json();
+  console.log(images);
+  return images[0];
+};
